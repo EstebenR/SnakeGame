020af1717bb7dd6da67578d62d1922e3ed032d8f
Esteban <estrestrepo+github@gmail.com>
2019-03-07 18:37:26 +0100

    Added initialize game function

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,8 +8,49 @@ namespace Snake
 {
     class Program
     {
+        struct Coor
+        {//Coordinate system
+            public int x;
+            public int y;
+        }
+        struct State
+        {
+            public char[,] matrix;
+            public Coor head;
+            public Coor direction;
+            public int points;
+        }
         static void Main(string[] args)
         {
+            State gameState = new State();
+
+            Console.Write("Game size: ");
+            int size = int.Parse(Console.ReadLine());
+            gameState.matrix = new char[size+2,size+2];
+
+            Initialize(ref gameState);
+        }
+
+
+        static void Initialize(ref State state)
+        {
+            int sideLength = state.matrix.GetLength(0);
+            //Fills the matrix with the sides and the empty background
+            for(int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    if (i == 0 | j == 0 | j == sideLength - 1 | i == sideLength - 1)
+                        state.matrix[i, j] = '#';
+                    else
+                        state.matrix[i, j] = '.';
+                }
+            }
+            //Sets the player position to the middle of the screen and its direction to the right
+            state.head = new Coor { x = sideLength / 2, y = sideLength / 2 };
+            state.matrix[state.head.x, state.head.y] = 's';
+            state.direction = new Coor { x = 0, y = 1 };
+            state.points = 0;
         }
     }
 }
